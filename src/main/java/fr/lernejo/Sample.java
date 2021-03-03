b57096d01a272c951cb2f4d98190d68dfc893ff5
ocruze <33525693+ocruze@users.noreply.github.com>
2021-03-03 11:49:56 +0100

    Add test to match 100% coverage

--- src/main/java/fr/lernejo/Sample.java
+++ src/main/java/fr/lernejo/Sample.java
@@ -4,6 +4,13 @@ import java.util.function.BiFunction;
 
 public class Sample {
 
+    public static int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            throw new DivisionByZeroException();
+
+        return (new Sample()).op(Operation.DIVIDE, dividend, divisor);
+    }
+
     public int op(Operation op, int a, int b) {
         return op.func.apply(a, b);
     }
@@ -11,6 +18,7 @@ public class Sample {
     enum Operation {
         ADD((a, b) -> a + b),
         MULT((a, b) -> a * b),
+        DIVIDE((a, b) -> a / b),
         ;
 
         final BiFunction<Integer, Integer, Integer> func;
